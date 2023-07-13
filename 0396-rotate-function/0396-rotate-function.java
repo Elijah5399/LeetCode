4d4d3011786aab116105242861ef6fd1a64cc7bc
Elijah Chia <chia.elijah@gmail.com>
2023-07-13 13:42:45 +0800

    Time: 1774 ms (5.37%), Space: 57.5 MB (77.81%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        LinkedList<Integer> l = new LinkedList<>();
+        int sum = 0;
+        int curr = 0;
+        int n = nums.length;
+        for (int i = 0; i <nums.length; i++) {
+            int num = nums[i];
+            curr += i * num;
+            sum += num;
+            l.add(num);
+        }
+        max = curr;
+        //System.out.println("curr is: " + curr);
+        for (int start = 1; start < n; start++) {
+            int popped = l.removeLast();
+            //System.out.println("popping: " + popped);
+            curr -= popped * (n - 1);
+            curr += (sum - popped);
+            l.addFirst(popped);
+            System.out.println("curr is: " + curr);
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
