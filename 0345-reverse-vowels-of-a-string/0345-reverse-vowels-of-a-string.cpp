f134778f56f79403abef6baab242f5fc4a5b7b36
Elijah Chia <chia.elijah@gmail.com>
2023-07-25 22:42:24 +0800

    Time: 14 ms (16.1%), Space: 8.3 MB (19%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n = s.size();
+        string vowels;
+        string res;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                vowels.push_back(s[i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                res.push_back(vowels.back());
+                vowels.pop_back();
+            } else {
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+    }
+};
